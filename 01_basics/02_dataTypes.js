d767b4a3b5dae9ffbb81aafec2f82215c6ab2ed8
Gulshan Kumar <110341457+Gulshan7282@users.noreply.github.com>
2023-09-10 17:13:09 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use Strict";   //treat all JS code as newer virsion
+
+// alert(3+3) // we are useing node js no browser
+
+console.log(3+3)
+console.log("Gulshan")
+
+let name = "Gulshan Kumar"
+let number = 18;
+let boolen = true
+
+//number => 2 to power 53
+//binint
+//string
+//boolean => true/false
+//null =>standlone value
+//undefined => not declear a value
+//symbole => unique
+
+
+//object
+
+console.log(typeof("Gulshan Kumer"))
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined 
