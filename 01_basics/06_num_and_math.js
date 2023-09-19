5cf54eecd14448f3476f43c820d7b0a3d774f20f
Gulshan Kumar <110341457+Gulshan7282@users.noreply.github.com>
2023-09-19 13:39:08 +0000

    done

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,22 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hunders = 10000000
+// console.log(hunders.toLocaleString('en-IN'));
+
+
+///+++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.abs(5));
+console.log(Math.round(4.356));
+console.log(Math.round(4.356));
