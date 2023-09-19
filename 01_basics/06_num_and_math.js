2349667bd789d2e4d4dcd73694793ae655c276a0
Gulshan Kumar <110341457+Gulshan7282@users.noreply.github.com>
2023-09-19 13:59:36 +0000

    math and num

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -15,8 +15,17 @@ const hunders = 10000000
 
 
 ///+++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++
-console.log(Math);
-console.log(Math.abs(-5));
-console.log(Math.abs(5));
-console.log(Math.round(4.356));
-console.log(Math.round(4.356));
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.abs(5));
+// console.log(Math.round(4.356));
+// console.log(Math.ceil(4.356));
+// console.log(Math.floor(4.356));
+
+console.log(Math.random());  // math.randoem() 0(zero) or 1 ke middle hi value date hai
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
